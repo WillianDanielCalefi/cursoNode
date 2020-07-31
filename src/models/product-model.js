ed1cdfface1bfe1ba9c84a3a57fcff2156151c5c
dtisistemas <dtisistemas@araras.sp.gov.br>
2020-07-31 14:56:31 -0300

    schema

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -3,5 +3,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const schema = new Schema({});
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [{
+        type: String,
+        required: true
+    }]
+});
 module.exports = mongoose.model('Product', Schema);
